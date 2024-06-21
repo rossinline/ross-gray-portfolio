075e3a326d8439870744f962e3c223ef9464e9c4
rossinline <rossg4196@gmail.com>
2024-06-21 14:58:27 +0100

    Deployed Website

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -23,7 +23,7 @@ export default {
           icon: "Home",
           full_title: "Property Contract Tracking Service",
           skills: "PHP,SQL,HTML,CSS,Tailwind CSS,JavaScript, Chart JS,JSON",
-          image_screenshot: new URL('@/assets/cat.jpg', import.meta.url).href, 
+          image_screenshot: "src/assets/cat.jpg, src/assets/wedding.jpg, src/assets/ruddocks.png", 
           feedback_name: "Conveyency Director",
           feedback_logo: "",
           feedback: "I could use this today, very real world!",
